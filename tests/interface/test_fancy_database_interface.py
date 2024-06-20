b7b8d8b8dc1f62f40ad25a9351943444de06c1d6
Tiexin Guo <guotiexin@gmail.com>
2024-06-20 14:47:23 +0800

    chore: try to delete redundant file

--- tests/interface/test_fancy_database_interface.py
+++
@@ -1,9 +0,0 @@
-from interface_tester import InterfaceTester
-
-
-def test_fancy_database_interface(interface_tester: InterfaceTester):
-    interface_tester.configure(
-        interface_name="my-fancy-database",
-        interface_version=0,
-    )
-    interface_tester.run()
